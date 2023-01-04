87a7fe77be94e48feb79d6cb032501acd6f45d69
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2023-01-03 20:48:26 -0700

    added Words.js to models

---
+++ models/Words.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Words extends Model {}
+
+Words.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        word: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        gallery_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'gallery',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'words'
+    }
+);
+
+module.exports = Words;
