f1d23506e70225a1251ef42494a4b1d8a8213bbb
Sean Collins <iseanc@hotmail.com>
2023-01-03 20:24:51 -0700

    Created Theme model

---
+++ models/Theme.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Theme extends Model {
+}
+
+Theme.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    theme_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'theme',
+  }
+);
+
+module.exports = Theme;
--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 const User = require('./User');
+const Theme = require('./Theme');
 
 // Add Sequelize associations (hasOne, hasMany, etc) here
 
 module.exports = {
   User,
 };
-
-module.exports = { User, };
