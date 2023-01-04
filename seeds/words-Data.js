0e57aac1a5a9d9c34b05b1044f15f316739e5faf
Sean Collins <iseanc@hotmail.com>
2023-01-04 09:23:33 -0700

    Changed references to words-Data to words-data.  Added more Words to words-data.js seed file so we have words for all 5 themes.

--- seeds/words-Data.js
+++ seeds/words-Data.js
@@ -1,4 +1,4 @@
-const { Words } = require('..models');
+const { Words } = require('../models');
 
 const wordsdata = [
     {
@@ -37,7 +37,82 @@ const wordsdata = [
         word: 'search',
         theme_id: 1,
      },
-
+     {
+        word: 'banana',
+        theme_id: 2,
+     },
+     {
+        word: 'almond',
+        theme_id: 2,
+     },
+     {
+        word: 'walnut',
+        theme_id: 2,
+     },
+     {
+        word: 'papaya',
+        theme_id: 2,
+     },
+     {
+        word: 'cherry',
+        theme_id: 2,
+     },
+     {
+        word: 'summer',
+        theme_id: 3,
+     },
+     {
+        word: 'winter',
+        theme_id: 3,
+     },
+     {
+        word: 'spring',
+        theme_id: 3,
+     },
+     {
+        word: 'autumn',
+        theme_id: 3,
+     },
+     {
+        word: 'aikido',
+        theme_id: 4,
+     },
+     {
+        word: 'squash',
+        theme_id: 4,
+     },
+     {
+        word: 'soccer',
+        theme_id: 4,
+     },
+     {
+        word: 'tennis',
+        theme_id: 4,
+     },
+     {
+        word: 'hockey',
+        theme_id: 4,
+     },
+     {
+        word: 'baboon',
+        theme_id: 5,
+     },
+     {
+        word: 'coyote',
+        theme_id: 5,
+     },
+     {
+        word: 'donkey',
+        theme_id: 5,
+     },
+     {
+        word: 'falcon',
+        theme_id: 5,
+     },
+     {
+        word: 'gerbil',
+        theme_id: 5,
+     },
 ]
 
 const seedWords = () => Words.bulkCreate(wordsdata);
