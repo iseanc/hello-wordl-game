e73eb5efdbb3b929f47196526da0b5a5f70c4666
Sean Collins <iseanc@hotmail.com>
2023-01-05 21:01:43 -0700

    craeted basic game-route files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,15 @@
 // NodeJS middleware for routes
 const router = require('express').Router();
 // Sequelize ORM models
-const { User } = require('../../models');
+const { User, Dictionary, Theme, Words } = require('../../models');
 // NodeJS module for filepaths
 const path = require('path');
 // Routes to manage User authentication activities (logon, new user, etc)
 const userRoutes = require('./user-routes');
+const gameRoutes = require('./game-routes.js');
 
 // URL path for user management
 router.use('/users', userRoutes);
+router.use('/game',gameRoutes);
 
 module.exports = router;
